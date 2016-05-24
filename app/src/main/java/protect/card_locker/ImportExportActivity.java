518f339c7afa9cd04ebfc9087bfbe6beba0390a4
Branden Archer <Branden Archer b.m.archer4@gmail.com>
2016-05-24 07:52:01 -0400

    Let pushing back button in menu end import/export activity

--- app/src/main/java/protect/card_locker/ImportExportActivity.java
+++ app/src/main/java/protect/card_locker/ImportExportActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -127,5 +128,19 @@ public class ImportExportActivity extends AppCompatActivity
             importExporter.cancel(true);
         }
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

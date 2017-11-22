559956025829dc16e5228f5b174740324f7f527b
Branden Archer <b.m.archer4@gmail.com>
2017-11-21 21:31:36 -0500

    Remove unneeded imports

--- app/src/main/java/protect/card_locker/ImportExportActivity.java
+++ app/src/main/java/protect/card_locker/ImportExportActivity.java
@@ -30,7 +30,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 public class ImportExportActivity extends AppCompatActivity
--- app/src/main/java/protect/card_locker/intro/IntroActivity.java
+++ app/src/main/java/protect/card_locker/intro/IntroActivity.java
@@ -1,12 +1,9 @@
 package protect.card_locker.intro;
 
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 
 import com.github.paolorotolo.appintro.AppIntro;
-
-import protect.card_locker.R;
 
 
 public class IntroActivity extends AppIntro

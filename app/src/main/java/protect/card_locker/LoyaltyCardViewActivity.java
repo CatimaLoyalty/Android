b5d41b0ab2f09c4a3f8389e1615b468b70f87cca
Sergio <sergio.paredes25@gmail.com>
2019-12-27 01:56:40 +0100

    #308 Display barcode format on display loyalty card view

--- app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
@@ -42,6 +42,8 @@ public class LoyaltyCardViewActivity extends AppCompatActivity
     TextView cardIdFieldView;
     TextView noteView;
     View noteViewDivider;
+    TextView barcodeFormatView;
+    View barcodeFormatViewDivider;
     TextView storeName;
     ImageView barcodeImage;
     View collapsingToolbarLayout;
@@ -107,6 +109,8 @@ public class LoyaltyCardViewActivity extends AppCompatActivity
         cardIdFieldView = findViewById(R.id.cardIdView);
         noteView = findViewById(R.id.noteView);
         noteViewDivider = findViewById(R.id.noteViewDivider);
+        barcodeFormatView = findViewById(R.id.barcodeFormatView);
+        barcodeFormatViewDivider = findViewById(R.id.barcodeFormatViewDivider);
         storeName = findViewById(R.id.storeName);
         barcodeImage = findViewById(R.id.barcode);
         collapsingToolbarLayout = findViewById(R.id.collapsingToolbarLayout);
@@ -269,10 +273,13 @@ public class LoyaltyCardViewActivity extends AppCompatActivity
                 Log.d(TAG, "ImageView size known known, creating barcode");
                 new BarcodeImageWriterTask(barcodeImage, cardIdString, format).execute();
             }
+            barcodeFormatView.setText(getString(R.string.barcodeTypePlaceholder, format));
         }
         else
         {
             findViewById(R.id.barcode).setVisibility(View.GONE);
+            barcodeFormatView.setVisibility(View.GONE);
+            barcodeFormatViewDivider.setVisibility(View.GONE);
         }
     }
 

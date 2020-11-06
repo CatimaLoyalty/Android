1bda5410d80d51a6cfa1a632c05a1dc266a94561
Sylvia van Os <sylvia@hackerchick.me>
2020-11-06 20:17:57 +0100

    Fix max db version

--- app/src/main/java/protect/card_locker/DBHelper.java
+++ app/src/main/java/protect/card_locker/DBHelper.java
@@ -13,7 +13,7 @@ public class DBHelper extends SQLiteOpenHelper
 {
     public static final String DATABASE_NAME = "Catima.db";
     public static final int ORIGINAL_DATABASE_VERSION = 1;
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 5;
 
     static class LoyaltyCardDbGroups
     {

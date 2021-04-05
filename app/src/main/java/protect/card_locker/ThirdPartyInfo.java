d347cdde3e4522c2c3a06127309a521aff20ca5d
Sylvia van Os <sylvia@hackerchick.me>
2021-04-05 19:43:49 +0200

    Add license info for third party things in About screen

---
+++ app/src/main/java/protect/card_locker/ThirdPartyInfo.java
@@ -0,0 +1,25 @@
+package protect.card_locker;
+
+public class ThirdPartyInfo {
+    private final String mName;
+    private final String mUrl;
+    private final String mLicense;
+
+    public ThirdPartyInfo(String name, String url, String license) {
+        mName = name;
+        mUrl = url;
+        mLicense = license;
+    }
+
+    public String name() {
+        return mName;
+    }
+
+    public String url() {
+        return mUrl;
+    }
+
+    public String license() {
+        return mLicense;
+    }
+}

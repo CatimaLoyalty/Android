cbcf1bcd998b9a5fdf7354a4287c64e95f2c3845
Sylvia van Os <sylvia@hackerchick.me>
2021-07-28 23:05:27 +0200

    Use desugaring to upgrade zxing without having to increase minSdk
    As documented by the zxing team on https://github.com/zxing/zxing/wiki/Frequently-Asked-Questions#it-doesnt-work-with-java-7--no-interface-method-sortljavautilcomparator

--- app/src/main/java/protect/card_locker/LoyaltyCardLockerApplication.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardLockerApplication.java
@@ -1,11 +1,10 @@
 package protect.card_locker;
 
-import android.app.Application;
-
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.multidex.MultiDexApplication;
 import protect.card_locker.preferences.Settings;
 
-public class LoyaltyCardLockerApplication extends Application {
+public class LoyaltyCardLockerApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
 

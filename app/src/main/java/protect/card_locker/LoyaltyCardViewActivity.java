18494078f92271b0f396bd09802b2cce0e627391
Ankit Tiwari <ankitr.tiwari@gmail.com>
2021-10-22 01:47:43 +0530

    Resolving conflict

--- app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
@@ -267,7 +267,7 @@ public class LoyaltyCardViewActivity extends CatimaAppCompatActivity implements 
                 if (imageTypes.get(mainImageIndex) == ImageType.BARCODE) {
                     redrawBarcodeAfterResize();
                 }
-                if(format!=null && format.isSquare())
+                if(loyaltyCard != null && format!=null && format.isSquare())
                     centerGuideline.setGuidelinePercent(0.75f * scale);
                 else
                     centerGuideline.setGuidelinePercent(0.5f * scale);

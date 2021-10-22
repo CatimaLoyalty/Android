6cb46d7fe72dfa7aa34fe93fba9143565447bccd
oreki27 <spandey97673@gmail.com>
2021-10-22 23:55:12 +0530

    Changed the non syncing code to get it synced on my device

--- app/src/main/java/protect/card_locker/Group.java
+++ app/src/main/java/protect/card_locker/Group.java
@@ -12,10 +12,10 @@ public class Group
         this.order = order;
     }
 
-    public static Group toGroup(Cursor cursor)
+    public static Group toGroup(Cursor cursor1)
     {
-        String _id = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.LoyaltyCardDbGroups.ID));
-        int order = cursor.getInt(cursor.getColumnIndexOrThrow(DBHelper.LoyaltyCardDbGroups.ORDER));
+        String _id = cursor1.getString(cursor1.getColumnIndexOrThrow(DBHelper.LoyaltyCardDbGroups.ID));
+        int order = cursor1.getInt(cursor1.getColumnIndexOrThrow(DBHelper.LoyaltyCardDbGroups.ORDER));
 
         return new Group(_id, order);
     }

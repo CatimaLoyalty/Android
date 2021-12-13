56813267dc268b3bd8e1b025f1274401621b2f80
Altonss <_>
2021-12-13 01:54:12 +0100

    Fix missing group error message: make groupWidget string not final

--- app/src/main/java/protect/card_locker/MainActivity.java
+++ app/src/main/java/protect/card_locker/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends CatimaAppCompatActivity implements LoyaltyCard
     private void extractIntentFields(Intent intent)
     {
         final Bundle b = intent.getExtras();
-        final String groupWidget = b != null ? b.getString("groupId") : null;
+        String groupWidget = b != null ? b.getString("groupId") : null;
         Log.d(TAG, "View activity: id=" + groupWidget);
     }
 

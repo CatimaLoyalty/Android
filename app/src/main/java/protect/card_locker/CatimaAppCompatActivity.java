26fd3e3a58246b98149036a8b5b4705eee62ada4
Sylvia van Os <sylvia@hackerchick.me>
2022-01-14 21:35:02 +0100

    Make spotBugs happy

--- app/src/main/java/protect/card_locker/CatimaAppCompatActivity.java
+++ app/src/main/java/protect/card_locker/CatimaAppCompatActivity.java
@@ -1,20 +1,10 @@
 package protect.card_locker;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.util.TypedValue;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
-import java.util.HashMap;
 
 public class CatimaAppCompatActivity extends AppCompatActivity {
-
-    SharedPreferences pref;
-    HashMap<String, Integer> supportedThemes;
-
     @Override
     protected void attachBaseContext(Context base) {
         // Apply chosen language

1c4b4f1f256d37cf2ed877b09744b85ee885de86
pooyanazari <azari.pooyan@gmail.com>
2022-06-06 21:58:29 +0430

    Set min date for expire date

--- app/src/main/java/protect/card_locker/LoyaltyCardEditActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardEditActivity.java
@@ -1234,7 +1234,16 @@ public class LoyaltyCardEditActivity extends CatimaAppCompatActivity {
             int day = c.get(Calendar.DAY_OF_MONTH);
 
             // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), this, year, month, day);
+            datePickerDialog.getDatePicker().setMinDate(getMinDateOfDatePicker());
+            return datePickerDialog;
+        }
+
+        private long getMinDateOfDatePicker()
+        {
+            Calendar minDateCalendar = Calendar.getInstance();
+            minDateCalendar.set(1970, 0, 1);
+            return minDateCalendar.getTimeInMillis();
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {

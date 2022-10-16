CatimaLoyalty/Android

commit ebedb43e727255eb522e8a8b07f2e66f7fa66eb9

issue-614 add receiver for share intent (#1078)

app/src/main/java/protect/card_locker/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.CursorIndexOutOfBoundsException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -18,17 +20,7 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -36,13 +28,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.view.ActionMode;
 import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -348,18 +341,9 @@ public class MainActivity extends CatimaAppCompatActivity implements LoyaltyCard
             Intent intent = result.getData();
             BarcodeValues barcodeValues = Utils.parseSetBarcodeActivityResult(Utils.BARCODE_SCAN, result.getResultCode(), intent, this);
 
-            if (!barcodeValues.isEmpty()) {
-                Intent newIntent = new Intent(getApplicationContext(), LoyaltyCardEditActivity.class);
-                Bundle newBundle = new Bundle();
-                newBundle.putString(LoyaltyCardEditActivity.BUNDLE_BARCODETYPE, barcodeValues.format());
-                newBundle.putString(LoyaltyCardEditActivity.BUNDLE_CARDID, barcodeValues.content());
-                Bundle inputBundle = intent.getExtras();
-                if (inputBundle != null && inputBundle.getString(LoyaltyCardEditActivity.BUNDLE_ADDGROUP) != null) {
-                    newBundle.putString(LoyaltyCardEditActivity.BUNDLE_ADDGROUP, inputBundle.getString(LoyaltyCardEditActivity.BUNDLE_ADDGROUP));
-                }
-                newIntent.putExtras(newBundle);
-                startActivity(newIntent);
-            }
+            Bundle inputBundle = intent.getExtras();
+            String group = inputBundle != null ? inputBundle.getString(LoyaltyCardEditActivity.BUNDLE_ADDGROUP) : null;
+            processBarcodeValues(barcodeValues, group);
         });
 
         mSettingsLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
@@ -511,9 +495,66 @@ public class MainActivity extends CatimaAppCompatActivity implements LoyaltyCard
         }
     }
 
+    private void processBarcodeValues(BarcodeValues barcodeValues, String group) {
+        if (barcodeValues.isEmpty()) {
+            return;
+        }
+        Intent newIntent = new Intent(getApplicationContext(), LoyaltyCardEditActivity.class);
+        Bundle newBundle = new Bundle();
+        newBundle.putString(LoyaltyCardEditActivity.BUNDLE_BARCODETYPE, barcodeValues.format());
+        newBundle.putString(LoyaltyCardEditActivity.BUNDLE_CARDID, barcodeValues.content());
+        if (group != null) {
+            newBundle.putString(LoyaltyCardEditActivity.BUNDLE_ADDGROUP, group);
+        }
+        newIntent.putExtras(newBundle);
+        startActivity(newIntent);
+    }
+
+    private void onSharedIntent(Intent intent) {
+        String receivedAction = intent.getAction();
+        String receivedType = intent.getType();
+
+        // Check if an image was shared to us
+        if (Intent.ACTION_SEND.equals(receivedAction)) {
+            if (receivedType.startsWith("image/")) {
+                BarcodeValues barcodeValues;
+                try {
+                    Bitmap bitmap;
+                    try {
+                        Uri data = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                        bitmap = Utils.retrieveImageFromUri(this, data);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Error getting data from image file");
+                        e.printStackTrace();
+                        Toast.makeText(this, R.string.errorReadingImage, Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+
+                    barcodeValues = Utils.getBarcodeFromBitmap(bitmap);
+
+                    if (barcodeValues.isEmpty()) {
+                        Log.i(TAG, "No barcode found in image file");
+                        Toast.makeText(this, R.string.noBarcodeFound, Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                } catch (NullPointerException e) {
+                    Toast.makeText(this, R.string.errorReadingImage, Toast.LENGTH_LONG).show();
+                    finish();
+                    return;
+                }
+                processBarcodeValues(barcodeValues, null);
+            } else {
+                Log.e(TAG, "Wrong mime-type");
+            }
+        }
+    }
+
     private void extractIntentFields(Intent intent) {
         final Bundle b = intent.getExtras();
         mArchiveMode = b != null && b.getBoolean(BUNDLE_ARCHIVE_MODE, false);
+        onSharedIntent(intent);
     }
 
     public void updateTabGroups(TabLayout groupsTabLayout) {

app/src/main/AndroidManifest.xml
@@ -33,6 +33,12 @@
 
                 <category android:name="android.intent.category.LAUNCHER" />
             </intent-filter>
+            <intent-filter>
+                <action android:name="android.intent.action.SEND" />
+
+                <category android:name="android.intent.category.DEFAULT" />
+                <data android:mimeType="image/*" />
+            </intent-filter>
         </activity>
         <activity
             android:name=".AboutActivity"

app/src/main/java/protect/card_locker/ScanActivity.java
@@ -12,6 +12,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.Intents;
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -20,11 +25,6 @@ import com.journeyapps.barcodescanner.CaptureManager;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
 import java.util.List;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.widget.Toolbar;
 
 import protect.card_locker.databinding.CustomBarcodeScannerBinding;
 import protect.card_locker.databinding.ScanActivityBinding;

app/src/main/java/protect/card_locker/Utils.java
@@ -11,6 +11,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.ImageDecoder;
 import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Build;
 import android.os.LocaleList;
 import android.provider.MediaStore;
@@ -18,6 +19,12 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.ColorUtils;
+import androidx.exifinterface.media.ExifInterface;
+import androidx.palette.graphics.Palette;
 
 import com.google.android.material.color.DynamicColors;
 import com.google.zxing.BinaryBitmap;
@@ -44,11 +51,6 @@ import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.Map;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.graphics.ColorUtils;
-import androidx.exifinterface.media.ExifInterface;
-import androidx.palette.graphics.Palette;
 import protect.card_locker.preferences.Settings;
 
 public class Utils {
@@ -118,12 +120,8 @@ public class Utils {
 
             Bitmap bitmap;
             try {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    ImageDecoder.Source image_source = ImageDecoder.createSource(context.getContentResolver(), intent.getData());
-                    bitmap = ImageDecoder.decodeBitmap(image_source, (decoder, info, source) -> decoder.setMutableRequired(true));
-                } else {
-                    bitmap = getBitmapSdkLessThan29(intent, context);
-                }
+                Uri data = intent.getData();
+                bitmap = retrieveImageFromUri(context, data);
             } catch (IOException e) {
                 Log.e(TAG, "Error getting data from image file");
                 e.printStackTrace();
@@ -163,9 +161,18 @@ public class Utils {
         throw new UnsupportedOperationException("Unknown request code for parseSetBarcodeActivityResult");
     }
 
+    static public Bitmap retrieveImageFromUri(Context context, Uri data) throws IOException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ImageDecoder.Source image_source = ImageDecoder.createSource(context.getContentResolver(), data);
+            return ImageDecoder.decodeBitmap(image_source, (decoder, info, source) -> decoder.setMutableRequired(true));
+        } else {
+            return getBitmapSdkLessThan29(data, context);
+        }
+    }
+
     @SuppressWarnings("deprecation")
-    private static Bitmap getBitmapSdkLessThan29(Intent intent, Context context) throws IOException {
-        return MediaStore.Images.Media.getBitmap(context.getContentResolver(), intent.getData());
+    private static Bitmap getBitmapSdkLessThan29(Uri data, Context context) throws IOException {
+        return MediaStore.Images.Media.getBitmap(context.getContentResolver(), data);
     }
 
     static public BarcodeValues getBarcodeFromBitmap(Bitmap bitmap) {

commit ea482c6fad25a0cbde99664d3a9919320b26a7c8

Fix crash on cancelling ScanActivity
Introduced in https://github.com/CatimaLoyalty/Android/pull/1078

app/src/main/java/protect/card_locker/MainActivity.java
@@ -338,6 +338,11 @@ public class MainActivity extends CatimaAppCompatActivity implements LoyaltyCard
          */
 
         mBarcodeScannerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            // Exit early if the user cancelled the scan (pressed back/home)
+            if (result.getResultCode() != RESULT_OK) {
+                return;
+            }
+
             Intent intent = result.getData();
             BarcodeValues barcodeValues = Utils.parseSetBarcodeActivityResult(Utils.BARCODE_SCAN, result.getResultCode(), intent, this);
 
@@ -497,8 +502,9 @@ public class MainActivity extends CatimaAppCompatActivity implements LoyaltyCard
 
     private void processBarcodeValues(BarcodeValues barcodeValues, String group) {
         if (barcodeValues.isEmpty()) {
-            return;
+            throw new IllegalArgumentException("barcodesValues may not be empty");
         }
+
         Intent newIntent = new Intent(getApplicationContext(), LoyaltyCardEditActivity.class);
         Bundle newBundle = new Bundle();
         newBundle.putString(LoyaltyCardEditActivity.BUNDLE_BARCODETYPE, barcodeValues.format());
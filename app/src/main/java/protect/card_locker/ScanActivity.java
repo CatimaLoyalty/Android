e97f7c8645c05e5e854c76724e9067f75b29bc8e
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-10-28 23:41:24 +0200

    Add support to pick image from content provider

--- app/src/main/java/protect/card_locker/ScanActivity.java
+++ app/src/main/java/protect/card_locker/ScanActivity.java
@@ -225,8 +225,13 @@ public class ScanActivity extends CatimaAppCompatActivity {
     public void addFromImage(View view) {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("image/*");
+        Intent contentIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        contentIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(photoPickerIntent, getString(R.string.addFromImage));
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { contentIntent });
         try {
-            photoPickerLauncher.launch(photoPickerIntent);
+            photoPickerLauncher.launch(chooserIntent);
         } catch (ActivityNotFoundException e) {
             Toast.makeText(getApplicationContext(), R.string.failedLaunchingPhotoPicker, Toast.LENGTH_LONG).show();
             Log.e(TAG, "No activity found to handle intent", e);

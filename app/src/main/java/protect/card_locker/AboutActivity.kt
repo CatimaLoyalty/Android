851acd85539fe6ffaeb11525d962abadbf57684e
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-10-30 22:06:31 +0100

    AboutActivity auto convert to kotlin

--- app/src/main/java/protect/card_locker/AboutActivity.kt
+++ app/src/main/java/protect/card_locker/AboutActivity.kt
@@ -1,99 +1,99 @@
-package protect.card_locker;
+package protect.card_locker
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+import protect.card_locker.AboutContent.pageTitle
+import protect.card_locker.AboutContent.copyright
+import protect.card_locker.AboutContent.versionHistory
+import protect.card_locker.AboutContent.destroy
+import protect.card_locker.OpenWebLinkHandler.open
+import protect.card_locker.AboutContent.contributorInfo
+import protect.card_locker.CatimaAppCompatActivity
+import protect.card_locker.AboutContent
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import protect.card_locker.OpenWebLinkHandler
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import protect.card_locker.R
+import protect.card_locker.databinding.AboutActivityBinding
 
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-
-
-import protect.card_locker.databinding.AboutActivityBinding;
-
-public class AboutActivity extends CatimaAppCompatActivity {
-
-    private static final String TAG = "Catima";
-
-    private AboutActivityBinding binding;
-    private AboutContent content;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = AboutActivityBinding.inflate(getLayoutInflater());
-        content = new AboutContent(this);
-        setTitle(content.getPageTitle());
-        setContentView(binding.getRoot());
-        setSupportActionBar(binding.toolbar);
-        enableToolbarBackButton();
-
-        TextView copyright = binding.creditsSub;
-        copyright.setText(content.getCopyright());
-        TextView versionHistory = binding.versionHistorySub;
-        versionHistory.setText(content.getVersionHistory());
-
-        binding.versionHistory.setTag("https://catima.app/changelog/");
-        binding.translate.setTag("https://hosted.weblate.org/engage/catima/");
-        binding.license.setTag("https://github.com/CatimaLoyalty/Android/blob/master/LICENSE");
-        binding.repo.setTag("https://github.com/CatimaLoyalty/Android/");
-        binding.privacy.setTag("https://catima.app/privacy-policy/");
-        binding.reportError.setTag("https://github.com/CatimaLoyalty/Android/issues");
-        binding.rate.setTag("https://play.google.com/store/apps/details?id=me.hackerchick.catima");
-
-        bindClickListeners();
+class AboutActivity : CatimaAppCompatActivity() {
+    private var binding: AboutActivityBinding? = null
+    private var content: AboutContent? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AboutActivityBinding.inflate(layoutInflater)
+        content = AboutContent(this)
+        title = content!!.pageTitle
+        setContentView(binding!!.root)
+        setSupportActionBar(binding!!.toolbar)
+        enableToolbarBackButton()
+        val copyright = binding!!.creditsSub
+        copyright.text = content!!.copyright
+        val versionHistory = binding!!.versionHistorySub
+        versionHistory.text = content!!.versionHistory
+        binding!!.versionHistory.tag = "https://catima.app/changelog/"
+        binding!!.translate.tag = "https://hosted.weblate.org/engage/catima/"
+        binding!!.license.tag = "https://github.com/CatimaLoyalty/Android/blob/master/LICENSE"
+        binding!!.repo.tag = "https://github.com/CatimaLoyalty/Android/"
+        binding!!.privacy.tag = "https://catima.app/privacy-policy/"
+        binding!!.reportError.tag = "https://github.com/CatimaLoyalty/Android/issues"
+        binding!!.rate.tag = "https://play.google.com/store/apps/details?id=me.hackerchick.catima"
+        bindClickListeners()
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
         if (id == android.R.id.home) {
-            finish();
+            finish()
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        content.destroy();
-        clearClickListeners();
-        binding = null;
+    override fun onDestroy() {
+        super.onDestroy()
+        content!!.destroy()
+        clearClickListeners()
+        binding = null
     }
 
-    private void bindClickListeners() {
-        View.OnClickListener openExternalBrowser = view -> {
-            Object tag = view.getTag();
-            if (tag instanceof String && ((String) tag).startsWith("https://")) {
-                (new OpenWebLinkHandler()).openBrowser(this, (String) tag);
+    private fun bindClickListeners() {
+        val openExternalBrowser = View.OnClickListener { view: View ->
+            val tag = view.tag
+            if (tag is String && tag.startsWith("https://")) {
+                OpenWebLinkHandler().open(this, tag)
             }
-        };
-        binding.versionHistory.setOnClickListener(openExternalBrowser);
-        binding.translate.setOnClickListener(openExternalBrowser);
-        binding.license.setOnClickListener(openExternalBrowser);
-        binding.repo.setOnClickListener(openExternalBrowser);
-        binding.privacy.setOnClickListener(openExternalBrowser);
-        binding.reportError.setOnClickListener(openExternalBrowser);
-        binding.rate.setOnClickListener(openExternalBrowser);
-
-        binding.credits.setOnClickListener(view -> showCredits());
+        }
+        binding!!.versionHistory.setOnClickListener(openExternalBrowser)
+        binding!!.translate.setOnClickListener(openExternalBrowser)
+        binding!!.license.setOnClickListener(openExternalBrowser)
+        binding!!.repo.setOnClickListener(openExternalBrowser)
+        binding!!.privacy.setOnClickListener(openExternalBrowser)
+        binding!!.reportError.setOnClickListener(openExternalBrowser)
+        binding!!.rate.setOnClickListener(openExternalBrowser)
+        binding!!.credits.setOnClickListener { view: View? -> showCredits() }
     }
 
-    private void clearClickListeners() {
-        binding.versionHistory.setOnClickListener(null);
-        binding.translate.setOnClickListener(null);
-        binding.license.setOnClickListener(null);
-        binding.repo.setOnClickListener(null);
-        binding.privacy.setOnClickListener(null);
-        binding.reportError.setOnClickListener(null);
-        binding.rate.setOnClickListener(null);
-        binding.credits.setOnClickListener(null);
+    private fun clearClickListeners() {
+        binding!!.versionHistory.setOnClickListener(null)
+        binding!!.translate.setOnClickListener(null)
+        binding!!.license.setOnClickListener(null)
+        binding!!.repo.setOnClickListener(null)
+        binding!!.privacy.setOnClickListener(null)
+        binding!!.reportError.setOnClickListener(null)
+        binding!!.rate.setOnClickListener(null)
+        binding!!.credits.setOnClickListener(null)
     }
 
-    private void showCredits() {
-        new MaterialAlertDialogBuilder(this)
-                .setTitle(R.string.credits)
-                .setMessage(content.getContributorInfo())
-                .setPositiveButton(R.string.ok, null)
-                .show();
+    private fun showCredits() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.credits)
+            .setMessage(content!!.contributorInfo)
+            .setPositiveButton(R.string.ok, null)
+            .show()
+    }
+
+    companion object {
+        private const val TAG = "Catima"
     }
 }

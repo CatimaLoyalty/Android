8f16942c05be456fd8da55e2958357b42efd0a40
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-10-30 22:11:44 +0100

    Optimize Kotlin code of AboutActivity

--- app/src/main/java/protect/card_locker/AboutActivity.kt
+++ app/src/main/java/protect/card_locker/AboutActivity.kt
@@ -1,20 +1,9 @@
 package protect.card_locker
 
-import protect.card_locker.AboutContent.pageTitle
-import protect.card_locker.AboutContent.copyright
-import protect.card_locker.AboutContent.versionHistory
-import protect.card_locker.AboutContent.destroy
-import protect.card_locker.OpenWebLinkHandler.open
-import protect.card_locker.AboutContent.contributorInfo
-import protect.card_locker.CatimaAppCompatActivity
-import protect.card_locker.AboutContent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
-import protect.card_locker.OpenWebLinkHandler
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import protect.card_locker.R
 import protect.card_locker.databinding.AboutActivityBinding
 
 class AboutActivity : CatimaAppCompatActivity() {
@@ -22,23 +11,31 @@ class AboutActivity : CatimaAppCompatActivity() {
     private var content: AboutContent? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = AboutActivityBinding.inflate(layoutInflater)
-        content = AboutContent(this)
-        title = content!!.pageTitle
-        setContentView(binding!!.root)
-        setSupportActionBar(binding!!.toolbar)
+
+        val binding = AboutActivityBinding.inflate(layoutInflater)
+            .also { this.binding = it }
+        val content = AboutContent(this)
+            .also { this.content = it }
+
+        title = content.pageTitle
+
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         enableToolbarBackButton()
-        val copyright = binding!!.creditsSub
-        copyright.text = content!!.copyright
-        val versionHistory = binding!!.versionHistorySub
-        versionHistory.text = content!!.versionHistory
-        binding!!.versionHistory.tag = "https://catima.app/changelog/"
-        binding!!.translate.tag = "https://hosted.weblate.org/engage/catima/"
-        binding!!.license.tag = "https://github.com/CatimaLoyalty/Android/blob/master/LICENSE"
-        binding!!.repo.tag = "https://github.com/CatimaLoyalty/Android/"
-        binding!!.privacy.tag = "https://catima.app/privacy-policy/"
-        binding!!.reportError.tag = "https://github.com/CatimaLoyalty/Android/issues"
-        binding!!.rate.tag = "https://play.google.com/store/apps/details?id=me.hackerchick.catima"
+
+        val copyright = binding.creditsSub
+        copyright.text = content.copyright
+        val versionHistory = binding.versionHistorySub
+        versionHistory.text = content.versionHistory
+
+        binding.versionHistory.tag = "https://catima.app/changelog/"
+        binding.translate.tag = "https://hosted.weblate.org/engage/catima/"
+        binding.license.tag = "https://github.com/CatimaLoyalty/Android/blob/master/LICENSE"
+        binding.repo.tag = "https://github.com/CatimaLoyalty/Android/"
+        binding.privacy.tag = "https://catima.app/privacy-policy/"
+        binding.reportError.tag = "https://github.com/CatimaLoyalty/Android/issues"
+        binding.rate.tag = "https://play.google.com/store/apps/details?id=me.hackerchick.catima"
+
         bindClickListeners()
     }
 
@@ -52,43 +49,49 @@ class AboutActivity : CatimaAppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        content!!.destroy()
+        content?.destroy()
+        content = null
         clearClickListeners()
         binding = null
     }
 
     private fun bindClickListeners() {
+        val binding = binding ?: return
+
         val openExternalBrowser = View.OnClickListener { view: View ->
             val tag = view.tag
             if (tag is String && tag.startsWith("https://")) {
                 OpenWebLinkHandler().open(this, tag)
             }
         }
-        binding!!.versionHistory.setOnClickListener(openExternalBrowser)
-        binding!!.translate.setOnClickListener(openExternalBrowser)
-        binding!!.license.setOnClickListener(openExternalBrowser)
-        binding!!.repo.setOnClickListener(openExternalBrowser)
-        binding!!.privacy.setOnClickListener(openExternalBrowser)
-        binding!!.reportError.setOnClickListener(openExternalBrowser)
-        binding!!.rate.setOnClickListener(openExternalBrowser)
-        binding!!.credits.setOnClickListener { view: View? -> showCredits() }
+
+        binding.versionHistory.setOnClickListener(openExternalBrowser)
+        binding.translate.setOnClickListener(openExternalBrowser)
+        binding.license.setOnClickListener(openExternalBrowser)
+        binding.repo.setOnClickListener(openExternalBrowser)
+        binding.privacy.setOnClickListener(openExternalBrowser)
+        binding.reportError.setOnClickListener(openExternalBrowser)
+        binding.rate.setOnClickListener(openExternalBrowser)
+        binding.credits.setOnClickListener { showCredits() }
     }
 
     private fun clearClickListeners() {
-        binding!!.versionHistory.setOnClickListener(null)
-        binding!!.translate.setOnClickListener(null)
-        binding!!.license.setOnClickListener(null)
-        binding!!.repo.setOnClickListener(null)
-        binding!!.privacy.setOnClickListener(null)
-        binding!!.reportError.setOnClickListener(null)
-        binding!!.rate.setOnClickListener(null)
-        binding!!.credits.setOnClickListener(null)
+        val binding = binding ?: return
+        binding.versionHistory.setOnClickListener(null)
+        binding.translate.setOnClickListener(null)
+        binding.license.setOnClickListener(null)
+        binding.repo.setOnClickListener(null)
+        binding.privacy.setOnClickListener(null)
+        binding.reportError.setOnClickListener(null)
+        binding.rate.setOnClickListener(null)
+        binding.credits.setOnClickListener(null)
     }
 
     private fun showCredits() {
+        val content = content ?: return
         MaterialAlertDialogBuilder(this)
             .setTitle(R.string.credits)
-            .setMessage(content!!.contributorInfo)
+            .setMessage(content.contributorInfo)
             .setPositiveButton(R.string.ok, null)
             .show()
     }

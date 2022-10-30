82d0160ec3e544af2b5c1d7404da3b9d862736f7
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-10-30 22:03:23 +0100

    OpenWebLinkHandler auto convert to kotlin

--- app/src/main/java/protect/card_locker/OpenWebLinkHandler.kt
+++ app/src/main/java/protect/card_locker/OpenWebLinkHandler.kt
@@ -1,29 +1,28 @@
-package protect.card_locker;
+package protect.card_locker
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
-import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.ActivityNotFoundException
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class OpenWebLinkHandler {
-
-    private static final String TAG = "Catima";
-
-    public void openBrowser(AppCompatActivity activity, String url) {
+class OpenWebLinkHandler {
+    fun open(activity: AppCompatActivity, url: String?) {
         if (url == null) {
-            return;
+            return
         }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
         try {
-            activity.startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(activity, R.string.failedToOpenUrl, Toast.LENGTH_LONG).show();
-            Log.e(TAG, "No activity found to handle intent", e);
+            activity.startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(activity, R.string.failedToOpenUrl, Toast.LENGTH_LONG).show()
+            Log.e(TAG, "No activity found to handle intent", e)
         }
+    }
+
+    companion object {
+        private const val TAG = "Catima"
     }
 }

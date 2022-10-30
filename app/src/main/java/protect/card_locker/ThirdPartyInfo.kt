a1b3061f8b92195a04f642873d5c8b3f7f4a4f57
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-10-30 22:31:42 +0100

    Convert ThirdPartyInfo to kotlin

--- app/src/main/java/protect/card_locker/ThirdPartyInfo.kt
+++ app/src/main/java/protect/card_locker/ThirdPartyInfo.kt
@@ -1,29 +1,11 @@
-package protect.card_locker;
+package protect.card_locker
 
-public class ThirdPartyInfo {
-    private final String mName;
-    private final String mUrl;
-    private final String mLicense;
-
-    public ThirdPartyInfo(String name, String url, String license) {
-        mName = name;
-        mUrl = url;
-        mLicense = license;
-    }
-
-    public String name() {
-        return mName;
-    }
-
-    public String url() {
-        return mUrl;
-    }
-
-    public String license() {
-        return mLicense;
-    }
-
-    public String toHtml() {
-        return String.format("<a href=\"%s\">%s</a> (%s)", url(), name(), license());
+data class ThirdPartyInfo(
+    val name: String,
+    val url: String,
+    val license: String
+) {
+    fun toHtml(): String {
+        return "<a href=\"$url\">$name</a> ($license)"
     }
 }

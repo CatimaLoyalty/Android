3116082c54abb734aee7e302c1238cb1b8be74b4
unknown <tomerpacific@gmail.com>
2022-11-02 23:15:24 +0200

    feature/add-percent-to-currency refactoring constructor for class and removing unused code

--- app/src/main/java/protect/card_locker/currency/CatimaCurrency.java
+++ app/src/main/java/protect/card_locker/currency/CatimaCurrency.java
@@ -7,55 +7,33 @@ import androidx.annotation.Nullable;
 public class CatimaCurrency {
 
     private Currency mCurrency;
-    private String mSpecialSymbol = "%";
+    private String mSpecialSymbol;
 
-    private static final String Points = "Points";
-    private static final String Percentage = "Percentage";
-    private static final String None = "";
+    private static final String Percent = "Percent";
 
-    public CatimaCurrency(Currency currency, String currencySymbol) {
+    public CatimaCurrency(Currency currency) {
         mCurrency = currency;
+        mSpecialSymbol = null;
+    }
+
+    public CatimaCurrency(String currencySymbol) {
         mSpecialSymbol = currencySymbol;
-    }
-
-    public CatimaCurrency fromCurrency(Currency currency) {
-        if (currency == null) {
-            mCurrency = null;
-            mSpecialSymbol = CatimaCurrency.None;
-        } else {
-            mCurrency = currency;
-            mSpecialSymbol = currency.getSymbol();
-        }
-
-        return this;
-    }
-
-    public CatimaCurrency fromCurrencyCode(String currencyCode) {
-        //Points
-        if (currencyCode == null) {
-            mCurrency = null;
-            mSpecialSymbol = CatimaCurrency.Points;
-        } else if (currencyCode.equals("%")) {
-            mCurrency = null;
-            mSpecialSymbol = CatimaCurrency.Percentage;
-        } else {
-            mCurrency = Currency.getInstance(currencyCode);
-            mSpecialSymbol = CatimaCurrency.None;
-        }
-
-        return this;
+        mCurrency = null;
     }
 
     public int getDefaultFractionDigits() {
         if (mCurrency != null) {
             return mCurrency.getDefaultFractionDigits();
-        } else if (mSpecialSymbol.equals(CatimaCurrency.Percentage)) {
-            return 2;
         }
+
         return 0;
     }
 
     public String getSymbol() {
+        if (mCurrency != null) {
+            return mCurrency.getSymbol();
+        }
+
         return mSpecialSymbol;
     }
 

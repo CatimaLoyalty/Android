9e6e802cd8f7716e5634f641ee210fe117748e8a
Swayam Khare <37830526+Swayam-Khare@users.noreply.github.com>
2023-06-02 09:10:50 +0530

    Removed unnecessary import
    Co-authored-by: FC Stegerman <flx@obfusk.net>

--- app/src/main/java/protect/card_locker/LoyaltyCardEditActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardEditActivity.java
@@ -33,7 +33,6 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 

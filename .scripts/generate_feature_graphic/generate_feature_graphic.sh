22622345f3dfab0c4d6a0b0b938fb6646fff96c0
Sylvia van Os <sylvia@hackerchick.me>
2023-11-07 21:14:19 +0100

    Support translating Catima app name in feature graphic

---
+++ .scripts/generate_feature_graphic/generate_feature_graphic.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+script_location="$(dirname $(readlink -f $0))"
+
+for lang in "$script_location/../../fastlane/metadata/android/"*; do
+  pushd "$lang"
+  # Place temporary copy for editing if needed
+  cp "$script_location/featureGraphic.svg" featureGraphic.svg
+  # Try splitting title.txt on — (em dash)
+  IFS='—' read -r appname subtext <<< $(cat title.txt)
+  if [ -z "$subtext" ]; then
+    # No result, try splitting on - (dash)
+    IFS='-' read -r appname subtext <<< $(cat title.txt)
+  fi
+  export appname
+  export subtext
+  # If there is subtext, change the .svg accordingly
+  if [ -n "$subtext" ]; then
+    perl -pi -e 's/Catima/$ENV{appname}/' featureGraphic.svg
+    perl -pi -e 's/Loyalty Card Wallet/$ENV{subtext}/' featureGraphic.svg
+    # Set correct font or font size for language if needed
+    # (Lexend Deca has limited support and some characters are big)
+    # We specifically need the Serif version because of the 200 weight
+    case "$(basename "$lang")" in
+      bg|el-GR|ru-RU|uk) sed -i "s/Lexend Deca/Noto Serif/" featureGraphic.svg ;;
+      ja-JP) sed -i "s/Lexend Deca/Noto Serif CJK JP/" featureGraphic.svg ;;
+      ko) sed -i "s/Lexend Deca/Noto Serif CJK KR/" featureGraphic.svg ;;
+      kn-IN) sed -i -e 's/font-size="150"/font-size="100"/' -e 's/y="285.511"/y="235.511"/' featureGraphic.svg ;;
+      zh-CN) sed -i "s/Lexend Deca/Noto Serif CJK SC/" featureGraphic.svg ;;
+      zh-TW) sed -i "s/Lexend Deca/Noto Serif CJK TC/" featureGraphic.svg ;;
+      *) ;;
+    esac
+  fi
+  # Ensure images directory exists
+  mkdir -p images
+  # Generate .png
+  convert featureGraphic.svg images/featureGraphic.png
+  # Optimize .png
+  optipng images/featureGraphic.png
+  # Remove metadata (timestamps) from .png
+  mat2 --inplace images/featureGraphic.png
+  # Remove temporary .svg
+  rm featureGraphic.svg
+  popd
+done

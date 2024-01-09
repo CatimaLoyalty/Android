2ae0ddd9e7b7ebf7bc9b03e612815d74681af06c
Sylvia van Os <sylvia@hackerchick.me>
2024-01-09 17:00:21 +0100

    Add option to always rotate

--- app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
@@ -226,7 +226,9 @@ public class LoyaltyCardViewActivity extends CatimaAppCompatActivity implements 
         settings = new Settings(this);
 
         String cardOrientation = settings.getCardViewOrientation();
-        if (cardOrientation.equals(getString(R.string.settings_key_lock_on_opening_orientation))) {
+        if (cardOrientation.equals(getString(R.string.settings_key_follow_sensor_orientation))) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        } else if (cardOrientation.equals(getString(R.string.settings_key_lock_on_opening_orientation))) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         } else if (cardOrientation.equals(getString(R.string.settings_key_portrait_orientation))) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);

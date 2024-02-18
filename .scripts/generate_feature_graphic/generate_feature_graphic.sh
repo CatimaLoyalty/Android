a906c685d4202548d6f76793cedf95d2a164fb70
Sylvia van Os <sylvia@hackerchick.me>
2024-02-18 14:56:35 +0100

    Fix hi-IN feature graphic generation

--- .scripts/generate_feature_graphic/generate_feature_graphic.sh
+++ .scripts/generate_feature_graphic/generate_feature_graphic.sh
@@ -24,7 +24,7 @@ for lang in "$script_location/../../fastlane/metadata/android/"*; do
     # (Lexend Deca has limited support and some characters are big)
     # We specifically need the Serif version because of the 200 weight
     case "$(basename "$lang")" in
-      bg|el-GR|ru-RU|uk) sed -i "s/Lexend Deca/Noto Serif/" featureGraphic.svg ;;
+      bg|el-GR|hi-IN|ru-RU|uk) sed -i "s/Lexend Deca/Noto Serif/" featureGraphic.svg ;;
       ja-JP) sed -i "s/Lexend Deca/Noto Serif CJK JP/" featureGraphic.svg ;;
       ko) sed -i "s/Lexend Deca/Noto Serif CJK KR/" featureGraphic.svg ;;
       kn-IN) sed -i -e 's/font-size="150"/font-size="100"/' -e 's/y="285.511"/y="235.511"/' featureGraphic.svg ;;

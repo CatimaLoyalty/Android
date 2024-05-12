aac9b59b7a082427c01597aa797bce017970a8af
Aayush Gupta <aayushgupta219@gmail.com>
2024-05-12 18:31:41 +0530

    Enable java language 17 features
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,12 +61,12 @@ android {
         // Flag to enable support for the new language APIs
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     sourceSets {

8419122193bf9ae3168d941f84e55f1d8de40840
Sylvia van Os <sylvia@hackerchick.me>
2025-09-10 23:24:36 +0200

    Fix wrong SDK_INT

--- app/src/main/java/protect/card_locker/ListWidget.kt
+++ app/src/main/java/protect/card_locker/ListWidget.kt
@@ -102,7 +102,7 @@ class ListWidget : AppWidgetProvider() {
             val icon = loyaltyCard.getImageThumbnail(context)
             // setImageViewIcon is not supported on Android 5, so force Android 5 down the text path
             // FIXME: The icon flow causes a crash up to Android 12L, so SDK_INT is forced up from 23 to 33
-            if (icon != null && Build.VERSION.SDK_INT >= 33) {
+            if (icon != null && Build.VERSION.SDK_INT >= 32) {
                 setInt(R.id.item_container_foreground, "setBackgroundColor", foreground)
                 setImageViewIcon(R.id.item_image, Icon.createWithBitmap(icon))
                 setViewVisibility(R.id.item_text, View.INVISIBLE)

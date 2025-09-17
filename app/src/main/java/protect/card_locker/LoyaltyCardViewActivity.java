e535a9ec7777abeed62c1f20be85f3d1bc1ee434
Sylvia van Os <sylvia@hackerchick.me>
2025-09-17 22:38:48 +0200

    Remove rotation support
    With the release of targetSdk 36, the Android platform will no longer
    always apply rotation. To lower the chance of user confusion, this
    support is being removed completely.
    
    I know some of you really love this, but I can't do anything about this.
    This is Google's decision and my opinion doesn't matter.

--- app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
+++ app/src/main/java/protect/card_locker/LoyaltyCardViewActivity.java
@@ -262,19 +262,6 @@ public class LoyaltyCardViewActivity extends CatimaAppCompatActivity implements 
 
         settings = new Settings(this);
 
-        String cardOrientation = settings.getCardViewOrientation();
-        if (cardOrientation.equals(getString(R.string.settings_key_follow_sensor_orientation))) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
-        } else if (cardOrientation.equals(getString(R.string.settings_key_lock_on_opening_orientation))) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
-        } else if (cardOrientation.equals(getString(R.string.settings_key_portrait_orientation))) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        } else if (cardOrientation.equals(getString(R.string.settings_key_landscape_orientation))) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        } else {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        }
-
         if (savedInstanceState != null) {
             mainImageIndex = savedInstanceState.getInt(STATE_IMAGEINDEX);
             isFullscreen = savedInstanceState.getBoolean(STATE_FULLSCREEN);

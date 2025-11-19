add92866b16bfbe451409d1fcdad98359a5ede75
LooKeR <iamlooker@proton.me>
2025-11-19 21:15:12 +0530

    refactor: Best practise apply theme as high as possible for most cases

--- app/src/main/java/protect/card_locker/AboutActivity.kt
+++ app/src/main/java/protect/card_locker/AboutActivity.kt
@@ -39,12 +39,14 @@ class AboutActivity : ComponentActivity() {
         title = content.pageTitle
 
         setContent {
-            AboutScreenContent(
-                content = content,
-                showDonate = BuildConfig.showDonate,
-                showRateOnGooglePlay = BuildConfig.showRateOnGooglePlay,
-                onBackPressedDispatcher = onBackPressedDispatcher
-            )
+            CatimaTheme {
+                AboutScreenContent(
+                    content = content,
+                    showDonate = BuildConfig.showDonate,
+                    showRateOnGooglePlay = BuildConfig.showRateOnGooglePlay,
+                    onBackPressedDispatcher = onBackPressedDispatcher
+                )
+            }
         }
     }
 
@@ -58,107 +60,105 @@ fun AboutScreenContent(
     showRateOnGooglePlay: Boolean = false,
     onBackPressedDispatcher: OnBackPressedDispatcher? = null,
 ) {
-    CatimaTheme {
-        Scaffold(
-            topBar = { CatimaTopAppBar(content.pageTitle.toString(), onBackPressedDispatcher) }
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier.padding(innerPadding).verticalScroll(rememberScrollState())
-            ) {
-                CatimaAboutSection(
-                    stringResource(R.string.version_history),
-                    content.versionHistory,
-                    modifier = Modifier.testTag("card_version_history"),
-                    onClickUrl = "https://catima.app/changelog/",
-                    onClickDialogText = AnnotatedString.fromHtml(
-                        htmlString = content.historyHtml,
-                        linkStyles = TextLinkStyles(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MaterialTheme.colorScheme.primary
-                            )
+    Scaffold(
+        topBar = { CatimaTopAppBar(content.pageTitle.toString(), onBackPressedDispatcher) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding).verticalScroll(rememberScrollState())
+        ) {
+            CatimaAboutSection(
+                stringResource(R.string.version_history),
+                content.versionHistory,
+                modifier = Modifier.testTag("card_version_history"),
+                onClickUrl = "https://catima.app/changelog/",
+                onClickDialogText = AnnotatedString.fromHtml(
+                    htmlString = content.historyHtml,
+                    linkStyles = TextLinkStyles(
+                        style = SpanStyle(
+                            textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colorScheme.primary
                         )
                     )
                 )
-                CatimaAboutSection(
-                    stringResource(R.string.credits),
-                    content.copyrightShort,
-                    modifier = Modifier.testTag("card_credits"),
-                    onClickDialogText = AnnotatedString.fromHtml(
-                        htmlString = content.contributorInfoHtml,
-                        linkStyles = TextLinkStyles(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MaterialTheme.colorScheme.primary
-                            )
+            )
+            CatimaAboutSection(
+                stringResource(R.string.credits),
+                content.copyrightShort,
+                modifier = Modifier.testTag("card_credits"),
+                onClickDialogText = AnnotatedString.fromHtml(
+                    htmlString = content.contributorInfoHtml,
+                    linkStyles = TextLinkStyles(
+                        style = SpanStyle(
+                            textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colorScheme.primary
                         )
                     )
                 )
-                CatimaAboutSection(
-                    stringResource(R.string.help_translate_this_app),
-                    stringResource(R.string.translate_platform),
-                    modifier = Modifier.testTag("card_translate"),
-                    onClickUrl = "https://hosted.weblate.org/engage/catima/"
-                )
-                CatimaAboutSection(
-                    stringResource(R.string.license),
-                    stringResource(R.string.app_license),
-                    modifier = Modifier.testTag("card_license"),
-                    onClickUrl = "https://github.com/CatimaLoyalty/Android/blob/main/LICENSE",
-                    onClickDialogText = AnnotatedString.fromHtml(
-                        htmlString = content.licenseHtml,
-                        linkStyles = TextLinkStyles(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MaterialTheme.colorScheme.primary
-                            )
+            )
+            CatimaAboutSection(
+                stringResource(R.string.help_translate_this_app),
+                stringResource(R.string.translate_platform),
+                modifier = Modifier.testTag("card_translate"),
+                onClickUrl = "https://hosted.weblate.org/engage/catima/"
+            )
+            CatimaAboutSection(
+                stringResource(R.string.license),
+                stringResource(R.string.app_license),
+                modifier = Modifier.testTag("card_license"),
+                onClickUrl = "https://github.com/CatimaLoyalty/Android/blob/main/LICENSE",
+                onClickDialogText = AnnotatedString.fromHtml(
+                    htmlString = content.licenseHtml,
+                    linkStyles = TextLinkStyles(
+                        style = SpanStyle(
+                            textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colorScheme.primary
                         )
                     )
                 )
-                CatimaAboutSection(
-                    stringResource(R.string.source_repository),
-                    stringResource(R.string.on_github),
-                    modifier = Modifier.testTag("card_source_github"),
-                    onClickUrl = "https://github.com/CatimaLoyalty/Android/"
-                )
-                CatimaAboutSection(
-                    stringResource(R.string.privacy_policy),
-                    stringResource(R.string.and_data_usage),
-                    modifier = Modifier.testTag("card_privacy_policy"),
-                    onClickUrl = "https://catima.app/privacy-policy/",
-                    onClickDialogText = AnnotatedString.fromHtml(
-                        htmlString = content.privacyHtml,
-                        linkStyles = TextLinkStyles(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.Underline,
-                                color = MaterialTheme.colorScheme.primary
-                            )
+            )
+            CatimaAboutSection(
+                stringResource(R.string.source_repository),
+                stringResource(R.string.on_github),
+                modifier = Modifier.testTag("card_source_github"),
+                onClickUrl = "https://github.com/CatimaLoyalty/Android/"
+            )
+            CatimaAboutSection(
+                stringResource(R.string.privacy_policy),
+                stringResource(R.string.and_data_usage),
+                modifier = Modifier.testTag("card_privacy_policy"),
+                onClickUrl = "https://catima.app/privacy-policy/",
+                onClickDialogText = AnnotatedString.fromHtml(
+                    htmlString = content.privacyHtml,
+                    linkStyles = TextLinkStyles(
+                        style = SpanStyle(
+                            textDecoration = TextDecoration.Underline,
+                            color = MaterialTheme.colorScheme.primary
                         )
                     )
                 )
-                if (showDonate) {
-                    CatimaAboutSection(
-                        stringResource(R.string.donate),
-                        "",
-                        modifier = Modifier.testTag("card_donate"),
-                        onClickUrl = "https://catima.app/donate"
-                    )
-                }
-                if (showRateOnGooglePlay) {
-                    CatimaAboutSection(
-                        stringResource(R.string.rate_this_app),
-                        stringResource(R.string.on_google_play),
-                        modifier = Modifier.testTag("card_rate_google"),
-                        onClickUrl = "https://play.google.com/store/apps/details?id=me.hackerchick.catima"
-                    )
-                }
+            )
+            if (showDonate) {
                 CatimaAboutSection(
-                    stringResource(R.string.report_error),
-                    stringResource(R.string.on_github),
-                    modifier = Modifier.testTag("card_report_error"),
-                    onClickUrl = "https://github.com/CatimaLoyalty/Android/issues"
+                    stringResource(R.string.donate),
+                    "",
+                    modifier = Modifier.testTag("card_donate"),
+                    onClickUrl = "https://catima.app/donate"
                 )
             }
+            if (showRateOnGooglePlay) {
+                CatimaAboutSection(
+                    stringResource(R.string.rate_this_app),
+                    stringResource(R.string.on_google_play),
+                    modifier = Modifier.testTag("card_rate_google"),
+                    onClickUrl = "https://play.google.com/store/apps/details?id=me.hackerchick.catima"
+                )
+            }
+            CatimaAboutSection(
+                stringResource(R.string.report_error),
+                stringResource(R.string.on_github),
+                modifier = Modifier.testTag("card_report_error"),
+                onClickUrl = "https://github.com/CatimaLoyalty/Android/issues"
+            )
         }
     }
 }

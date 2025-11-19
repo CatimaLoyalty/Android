5aac762cd5eb999c1c677645897ed44c637f870b
LooKeR <iamlooker@proton.me>
2025-11-19 20:59:12 +0530

    test: Add basic test for compose about screen

---
+++ app/src/androidTest/java/protect/card_locker/ComposeAboutActivityTest.kt
@@ -0,0 +1,43 @@
+package protect.card_locker
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import org.junit.Rule
+import org.junit.Test
+
+class ComposeAboutActivityTest {
+    @get:Rule
+    val rule: ComposeContentTestRule = createComposeRule()
+
+    @Test
+    fun testPasses(): Unit = with(rule) {
+        setContent {
+            AboutScreenContent(
+                content = AboutContent(LocalContext.current),
+                showDonate = true,
+                showRateOnGooglePlay = false,
+                onBackPressedDispatcher = null
+            )
+        }
+
+        onNodeWithTag("topbar_catima").assertIsDisplayed()
+    }
+
+    @Test
+    fun testFails(): Unit = with(rule) {
+        setContent {
+            AboutScreenContent(
+                content = AboutContent(LocalContext.current),
+                showDonate = true,
+                showRateOnGooglePlay = false,
+                onBackPressedDispatcher = null
+            )
+        }
+
+        onNodeWithTag("topbar_catima").assertIsNotDisplayed()
+    }
+}

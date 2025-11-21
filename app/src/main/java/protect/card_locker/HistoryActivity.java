9dfbff85bd73798a85ea44dc41e94e21286af826
bsp280406 <bsp280406@gmail.com>
2025-11-21 11:58:08 +0000

    did some modifications

--- app/src/main/java/protect/card_locker/HistoryActivity.java
+++ app/src/main/java/protect/card_locker/HistoryActivity.java
@@ -1,6 +1,8 @@
 package protect.card_locker;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import java.util.Objects;
 
 public class HistoryActivity extends CatimaAppCompatActivity {
 
@@ -8,6 +10,11 @@ public class HistoryActivity extends CatimaAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_activity);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
         setTitle(R.string.history_title);
 
         final DBHelper dbHelper = new DBHelper(this);

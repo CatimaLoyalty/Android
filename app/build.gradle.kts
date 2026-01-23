38a2c3f26b5359de34c0c0d0e97d7da77afcb156
Sylvia van Os <sylvia@hackerchick.me>
2026-01-23 21:37:11 +0100

    Revert "Reapply "Temporarily re-add Android 5 support (#2925)""

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "me.hackerchick.catima"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 163
         versionName = "2.41.6"

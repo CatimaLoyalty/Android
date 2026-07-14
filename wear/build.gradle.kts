603db4732acc76f8f09d87e7371958aef51ceb7b
Fabio Manganiello <fabio@manganiello.tech>
2026-07-14 11:05:56 +0200

    chore(wear): Remove Java 21 compileOptions from Wear Gradle config
    Redundant, since `jvmToolchain` is already specified.
    
    Addresses: https://github.com/CatimaLoyalty/Android/pull/3185#discussion_r3544445993

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -37,11 +37,6 @@ android {
     buildFeatures {
         compose = true
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
 }
 
 dependencies {
